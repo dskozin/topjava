67d5e607a3fea12f29f6bd253e3cd81cad61425f
Dmitriy Kozin <dskozin@mail.ru>
2018-03-30 11:25:07 +0300

    lesson 4

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,15 +1,41 @@
 package ru.javawebinar.topjava.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id = :id AND m.user.id = :userId"),
+        @NamedQuery(name = Meal.ALL_SORTED, query = "SELECT m FROM Meal m WHERE m.user.id = :userId ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.GET_BETWEEN, query = "SELECT m FROM Meal m WHERE m.user.id = :userId  AND m.dateTime BETWEEN  :startDateTime AND :endDateTime ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.GET_BY_ID, query = "SELECT m FROM Meal m WHERE m.id = :id AND m.user.id = :userId")
+})
+@Entity
+@Table(name = "meals")
 public class Meal extends AbstractBaseEntity {
+
+    public static final String DELETE = "Meal.Delete";
+    public static final String ALL_SORTED = "Meal.AllSorted";
+    public static final String GET_BETWEEN = "Meal.GetBetween";
+    public static final String GET_BY_ID = "Meal.BetById";
+
+    @Column(name ="date_time")
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name ="description")
+    @NotNull
     private String description;
 
+    @Column(name = "calories")
+    @NotNull
     private int calories;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public Meal() {
     }
@@ -55,6 +81,14 @@ public class Meal extends AbstractBaseEntity {
 
     public void setCalories(int calories) {
         this.calories = calories;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     @Override
